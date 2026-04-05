07e595ae0d4110ccf8854ae44433dee55aac71f1
Taha Aflouk <tahaaflouk94@gmail.com>
2026-04-05 01:21:28 +0100

    initial commit: notification service setup

---
+++ src/test/java/com/sigma/smarthome/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.smarthome.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
